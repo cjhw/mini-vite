049e05a242e02cc5a8a699719ccb21893a8065f5
caijianhaoa <1784581164@qq.com>
2022-09-12 15:28:36 +0800

    实现hmr服务端

--- src/node/server/index.ts
+++ src/node/server/index.ts
@@ -9,6 +9,9 @@ import { indexHtmlMiddware } from "./middlwares/indexHtml";
 import { transformMiddleware } from "./middlwares/transform";
 import { staticMiddleware } from "./middlwares/static";
 import { ModuleGraph } from "../ModuleGraph";
+import chokidar, { FSWatcher } from "chokidar";
+import { createWebSocketServer } from "../ws";
+import { bindingHMREvents } from "../hmr";
 
 export interface ServerContext {
   root: string;
@@ -16,6 +19,8 @@ export interface ServerContext {
   app: connect.Server;
   plugins: Plugin[];
   moduleGraph: ModuleGraph;
+  ws: { send: (data: any) => void; close: () => void };
+  watcher: FSWatcher;
 }
 
 export async function startDevServer() {
@@ -25,6 +30,13 @@ export async function startDevServer() {
   const plugins = resolvePlugins();
   const moduleGraph = new ModuleGraph((url) => pluginContainer.resolveId(url));
   const pluginContainer = createPluginContainer(plugins);
+  const watcher = chokidar.watch(root, {
+    ignored: ["**/node_modules/**", "**/.git/**"],
+    ignoreInitial: true,
+  });
+
+  // WebSocket 对象
+  const ws = createWebSocketServer(app);
 
   const serverContext: ServerContext = {
     root: process.cwd(),
@@ -32,7 +44,11 @@ export async function startDevServer() {
     pluginContainer,
     plugins,
     moduleGraph,
+    ws,
+    watcher,
   };
+
+  bindingHMREvents(serverContext);
 
   for (const plugin of plugins) {
     if (plugin.configureServer) {
