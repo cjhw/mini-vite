0ffcc74606c3fae0db95f80395cffc7b21300745
caijianhaoa <1784581164@qq.com>
2022-10-13 12:17:25 +0800

    playground没有引用到react导致没有扫描到，没有对react进行于构建，修改一下

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -4,7 +4,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div>123</div>;
+  const [count, setCount] = useState(1234);
+  return <div>{count}</div>;
 }
 
 export default App;
--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
-
-const App = () => <div>hello 1231</div>;
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
